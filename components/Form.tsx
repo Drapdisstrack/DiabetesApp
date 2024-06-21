897189d99fee4b6a5048323ec178ca4affd8d65a
Aranza-Ricalde <studioghibli8888@gmail.com>
2024-06-21 00:09:44 -0600

    refactor

--- components/Form.tsx
+++ components/Form.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { View, TextInput, StyleSheet, TouchableOpacity, Text } from "react-native";
+import { View, TextInput, TouchableOpacity, Text } from "react-native";
 import { Ionicons } from '@expo/vector-icons';
 import { fontStyle } from "@/constants/FontStyles";
 import { buttonStyles } from "@/constants/Buttons";
